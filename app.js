94fe9bf622d67bf67f5b030d7aa2a3efa8594fa7
Hominsec <robinsond993@gmail.com>
2023-11-12 20:24:09 -0500

    Exercise 2

---
+++ app.js
@@ -0,0 +1,22 @@
+window.addEventListener('load',()=>{
+    let searchbutton=document.querySelector("#btn");
+
+    const httpRequest = new XMLHttpRequest();
+    let url = "superheroes.php";
+    httpRequest.onreadystatechange = listheroes;
+    httpRequest.open('GET', url);
+    httpRequest.send();
+
+    function listheroes() {
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+         if (httpRequest.status === 200) {
+         let response = httpRequest.responseText;
+         alert(response);
+         } else {
+         alert('There was a problem with the request.');
+         }
+        }
+        }
+});
+
+ 
