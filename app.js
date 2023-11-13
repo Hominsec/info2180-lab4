9ca8347f8f57749c0a7809e1974c10b8375d0ee8
Hominsec <robinsond993@gmail.com>
2023-11-12 22:56:23 -0500

    Final Code

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 window.addEventListener('load',()=>{
     let searchbutton=document.querySelector(".btn");
-
+    let data=document.getElementById("input_form");
+    let output=document.getElementById("result");
     const httpRequest = new XMLHttpRequest();
-    let url = "superheroes.php";
+    
+    //let url = "superheroes.php";
 
     searchbutton.addEventListener("click",()=>{
+        //let filtered=data.value.trim();
+        let url = "http://localhost/info2180-lab4/superheroes.php?query="+data.value;
+        //console.log(data.value);
         httpRequest.onreadystatechange = listheroes;
         httpRequest.open('GET', url);
         httpRequest.send();  
@@ -12,10 +17,13 @@ window.addEventListener('load',()=>{
     
 
     function listheroes() {
+        
         if (httpRequest.readyState === XMLHttpRequest.DONE) {
          if (httpRequest.status === 200) {
          let response = httpRequest.responseText;
-         alert(response);
+         //alert(response);
+         //console.log(response);
+         output.innerHTML=response;
          } else {
          alert('There was a problem with the request.');
          }
